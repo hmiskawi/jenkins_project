d298ce4aebeae111c5ef39d65b49c74b39c85860
hmiskawi <hhm30@mail.aub.edu>
2024-11-16 19:29:24 +0200

    Minor modifications

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from Hassan Miskawi!")
+        self.assertEqual(greet("Hassan Miskawi"), "Hello, World from Hassan Miskawi!")
 
 if __name__ == "__main__":
     unittest.main()
